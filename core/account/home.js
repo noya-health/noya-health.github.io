1fda748370541b756d1b2e973386f119328fb36c
bryanlowzh1 <bryanlowzh@gmail.com>
2021-02-14 10:38:48 +0800

    Update home.js

--- core/account/home.js
+++ core/account/home.js
@@ -7,49 +7,56 @@ $('a[href="#"]').click(function (event) {
 });
 
 /*----------- Modal -----------*/
-
 // Open modal
 function openModal(origin) {
+    // Blur + opacity
     $(".section-modal").addClass("show visible");
+
+    // Lock body scroll
     $('.body-content').css('overflow', 'hidden');
 
     var originId = origin.attr('id')
+
+    // Declare first
     var modalContainerId
     var modalContainer
 
     if (originId.startsWith('btn-modal')) {
+        // If modal triggered by btn-modal, find the relevant modal container
         modalContainerId = origin.attr('dm-container');
         modalContainer = $('#' + modalContainerId);
+        // If opening modal within a modal -> closest preceding modal
         origin.closest('.modal-container').removeClass("show");
     } else if (originId.startsWith('mc-pageload')) {
+        // For modal loading upon page visit
         modalContainerId = origin.attr('id');
         modalContainer = $('#' + modalContainerId);
     }
-    
+
     modalContainer.addClass('show')
 
+    // Autoclose success/unsuccessful feedback modals
     if (modalContainerId.startsWith('mc-load') || modalContainerId.startsWith('mc-pageload')) {
         autocloseModal()
     } else {
     }
 
+    // Plays animation
     if (modalContainer.is('[anim-id]')) {
         var animId = modalContainer.attr('anim-id')
         if ($(window).width() > 991) {
+            // Timeout ≈ desktop modal transition time
             setTimeout(function () {
                 animFuncs[animId]();
             }, 210);
         } else {
+            // Timeout ≈ mobile modal transition time
             setTimeout(function () {
                 animFuncs[animId]();
             }, 410);
         }
     }
 }
-
-$(window).on("load", function () {
-    openModal($('#mc-pageload-success-plan-1'))
-});
 
 $("[id^='btn-modal']").on('click', function () {
     openModal($(this))
@@ -77,12 +84,10 @@ function closeModal() {
                 var animId = $(this).attr('anim-id');
                 if ($(window).width() > 991) {
                     setTimeout(function () {
-                        // Triggers error log in console but works fine
                         animResetFuncs[animId]()
                     }, 200)
                 } else {
                     setTimeout(function () {
-                        // Triggers error log in console but works fine
                         animResetFuncs[animId]()
                     }, 400)
                 }
@@ -177,7 +182,12 @@ function showScrolledTitle(modalSection) {
     }
 }
 
-/*----------- Animations -----------*/
+/*----------- Pageload -----------*/
+// Loads plan purchase success modal
+$(window).on("load", function () {
+    openModal($('#mc-pageload-success-plan-1'))
+});
+
 // Animation objects
 var animFuncs = {
     'home-plansuccess-1': function () {
